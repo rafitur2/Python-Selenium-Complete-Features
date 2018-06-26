f6e11472c3aa54d5e48190246969f4be5127e248
Rafi Diamant <rafi.diamant@amdocs.com>
2018-06-26 09:34:20 +0300

    comment

--- Test_Selenium.py
+++ Test_Selenium.py
@@ -1,4 +1,29 @@
 from selenium import webdriver
+import time
+import unittest
+
+wd = webdriver.Chrome()
+
+class TestSelenium(unittest.TestCase):
+
+    def test_elements(self):
+        wd.get(("http://www.teachmeselenium.com/automation-practice/"))
+        #Find element by locator ID and populate the first name
+        _first_name_element= wd.find_element_by_id("firstname")
+        _first_name_element.send_keys("Eddi")
+
+        #Radio Buttons
+        _gender = wd.find_element_by_xpath("//input[@name='gender' and @value = 'Male']")
+        _gender.click()
 
 
-wd = webdriver.chrome()
+
+
+
+        time.sleep(2)
+        wd.quit()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
